e25b783a0d4b1503d103d6cdb20f66ecb38dcffe
rahul-sarker18 <rahulsarker1495@gmail.com>
2023-03-15 19:29:29 +0530

    data delet and update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 8000;
@@ -21,66 +21,90 @@ client.connect((err) => {
 });
 
 async function run() {
-    try {
-      const db = client.db("storekoall");
-      const user = db.collection("user")
+  try {
+    const db = client.db("storekoall");
+    const user = db.collection("user");
+    const marqe = db.collection("marqe");
+    const allPhoto = db.collection("allphoto");
 
-// *************get data **************************************************
-        app.get('/user', async(req , res)=>{
-        
-          const result = await user.find({}).toArray()
-          res.send(result)
-        })
+    // *************get data **************************************************
+    // user get
+    app.get("/images", async (req, res) => {
+      const useremail = req.query.email;
+      const query = { email: useremail };
+      const result = await allPhoto.find(query).toArray();
+      res.send(result);
+    });
+    // user get
+    app.get("/user", async (req, res) => {
+      const useremail = req.query.email;
+      const query = { email: useremail };
+      const result = await user.findOne(query);
+      res.send(result);
+    });
+    // slider data get
+    app.get("/marqe", async (req, res) => {
+      const result = await marqe.find({}).toArray();
+      res.send(result);
+    });
 
+    // ************** post data -************************************
+    app.post("/userPost", async (req, res) => {
+      const data = req.body;
+      const result = await user.insertOne(data);
+      res.send(result);
+    });
+    // post image
+    app.post("/postImage", async (req, res) => {
+      const data = req.body;
+      const result = await allPhoto.insertOne(data);
+      res.send(result);
+    });
 
-      
-// ************** post data -************************************
-            app.post('/userPost', async(req , res)=>{
-                const data = req.body;
-                const result = await user.insertOne(data);
-                res.send(result)
-            })
+    // *********************** delete ***************************
+    app.delete("/imagede/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await allPhoto.deleteOne(query);
+      res.send(result);
+    });
 
+    // ******************** update data**********************
+    // user post in dt
 
-
-
-      // user post in dt
-     
-    //   // update like and count
-    //   app.put("/like/:id", async (req, res) => {
-    //     const bodyemail = req.body;
-    //     const email = bodyemail.email;
-    //     const id = req.params.id;
-    //     const getpostquery = { _id: ObjectId(id) };
-  
-    //     const getpost = await myposts.findOne(getpostquery);
-    //     const { like } = getpost;
-  
-    //     for (const aa of like) {
-    //       console.log(aa);
-    //       if (aa === email) {
-    //         console.log('object');
-    //         return;
-    //       }
-            
-    //     }
-  
-    //     const option = { upsert: true };
-    //     const updaterev = {
-    //       $set: {
-    //         like: [...like ,email],
-    //       },
-    //     };
-    //     const result = await myposts.updateOne(getpostquery, updaterev, option);
-    //     res.send(result);
-  
-    //   });
-  
-    } finally {
-    }
+    //   // update name
+    app.put("/username", async (req, res) => {
+      const emailUser = req.query.email;
+      const { name } = req.body;
+      const query = { email: emailUser };
+      const option = { upsert: true };
+      const updaterev = {
+        $set: {
+          name: name,
+        },
+      };
+      const result = await user.updateOne(query, updaterev, option);
+      res.send(result);
+    });
+    //   // update img
+    app.put("/userPut", async (req, res) => {
+      const emailUser = req.query.email;
+      const { img } = req.body;
+      const query = { email: emailUser };
+      const option = { upsert: true };
+      const updaterev = {
+        $set: {
+          image: img,
+        },
+      };
+      const result = await user.updateOne(query, updaterev, option);
+      res.send(result);
+    });
+  } finally {
   }
-  
-  run().catch(console.dir);
+}
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("okkkk");
